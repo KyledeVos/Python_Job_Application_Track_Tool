50682cefca69ecfe74e0420419249059546de87f
Kyle <kdevos827@gmail.com>
2024-01-30 11:38:19 +0200

    Modified Database controller to call for table creation using a specified database initializer class

--- controllers/database_controller.py
+++ controllers/database_controller.py
@@ -1,6 +1,19 @@
-
+import sqlite3
+from persistence_modules.parents.initialize_db_parent import InitializeDbParent
 
 class DatabaseController():
 
-    def __init__(self, database) -> None:
+    def __init__(self, database, InitializeDbParent) -> None:
         self.database = database
+        self.connection = None
+        self.cursor = None
+        self.db_initializer = InitializeDbParent
+
+    def create_all_tables(self):
+
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
+
+        self.db_initializer.create_all_tables(self.connection, self.cursor)
+
+        self.connection.close()
