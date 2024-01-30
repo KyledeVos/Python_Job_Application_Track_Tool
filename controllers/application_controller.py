30a03631f8b55379baae565cd18ca76dafc677d6
Kyle <kdevos827@gmail.com>
2024-01-30 11:39:22 +0200

    modified application controller to specify database initializer and added start_app method

--- controllers/application_controller.py
+++ controllers/application_controller.py
@@ -1,9 +1,20 @@
+# import sub controllers
 from .screen_controller import ScreenController
 from .database_controller import DatabaseController
+
+# DATABASE COMPONENTS
+# -------------------------------------------------
+from persistence_modules.db_initializer import DbInitializer
 
 class ApplicationController():
 
     def __init__(self, database) -> None:
+        print("application controller initialize")
+        self.db_initializer = DbInitializer()
         
+        # initialize controllers
         self.screen_controller = ScreenController()
-        self.database_controller = DatabaseController(database)
+        self.database_controller = DatabaseController(database, self.db_initializer)
+
+    def start_app(self):
+        self.database_controller.create_all_tables()
