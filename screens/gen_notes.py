d23bc350bc13db62ae97ea01475e68031f12ee9a
Kyle <kdevos827@gmail.com>
2024-01-31 11:54:48 +0200

    Added Screen for General Notes

---
+++ screens/gen_notes.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from .parent_screens import FullScreen
+
+class GeneralNotesScreen(FullScreen):
+
+    def __init__(self, container):
+        super().__init__(container)
+        
+        self.window_title = Label(self.screen_window, text = "General Notes")
+        self.screen_window.grid_columnconfigure(0, weight=1)
+        self.window_title.grid(row=0, column=0)
+
+
+
+
+
