12d85cd8ae9d11fa83968faacf3eb38ad88ff1af
Kyle <kdevos827@gmail.com>
2024-02-01 08:56:52 +0200

    Added Sectioned Parent Screen - right is major screen

--- screens/parent_screens.py
+++ screens/parent_screens.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 class FullScreen():
 
-    def __init__(self, container):
+    def __init__(self, container) -> None:
         self.screen_window = LabelFrame(container)
     
     def get_labelFrame(self):
@@ -10,5 +10,43 @@ class FullScreen():
     
     def load_window(self):
         self.screen_window.grid(row=0, column=0, sticky="NEWS")
+
+
+class SectionedLeftMinor():
+
+    def __init__(self, container, frame_title) -> None:
+        self.enclosing_window = LabelFrame(container)
+        self.left_minor_window = LabelFrame(self.enclosing_window, bg='red')
+        self.right_major_window = LabelFrame(self.enclosing_window, bg='purple')
+
+        self.configure_sub_windows(frame_title)
+
+    def configure_sub_windows(self, frame_title):
+        
+        
+        self.enclosing_window.grid_columnconfigure(0, weight = 1)
+        self.enclosing_window.grid_columnconfigure(1, weight=3)
+        self.enclosing_window.grid_rowconfigure(1, weight = 1)
+            
+        self.window_title = Label(self.enclosing_window, text = frame_title )
+        self.window_title.grid(row=0, column=0, columnspan=2, sticky="NEWS")
+
+        self.left_minor_window.grid(row = 1, column= 0, sticky="NEWS")
+        self.right_major_window.grid(row = 1, column = 1, sticky="NEWS")
+
+
+    def get_enclosing_window(self):
+        return self.enclosing_window
+
+
+    def get_left_minor(self):
+        return self.left_minor_window
+    
+
+    def get_right_major(self):
+        return self.right_major_window
+    
+
+
     
 
