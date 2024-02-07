e18d8e078cf1e3581a5686e91d892e51aefda8c4
Kyle <kdevos827@gmail.com>
2024-02-07 15:29:09 +0200

    Added new function to retrieve and configure specific job data

--- persistence_modules/db_reader.py
+++ persistence_modules/db_reader.py
@@ -4,12 +4,18 @@ class DbReader():
 
     def __init__(self):
         pass
+    
+    # ----------------------------------------------------------------------------------
+    # Generic Functions
 
     def retrieve_all_data(self, cursor, table_name):
         return cursor.execute(f"SELECT * FROM {table_name}").fetchall()
     
     def retrieve_single_col_list(self, cursor, column, table_name):
         return [item[0] for item in cursor.execute(f"SELECT {column} FROM {table_name}").fetchall()]
+    
+    def retrieve_single_col(self, cursor, id, column_name, table_name):
+        return cursor.execute(f"SELECT {column_name} FROM {table_name} WHERE id={id}").fetchone()
 
     def retrieve_id_single_col(self, cursor, column, table_name):
          return cursor.execute(f"SELECT id, {column} FROM {table_name}").fetchall()
@@ -18,7 +24,7 @@ class DbReader():
         return cursor.execute(f"SELECT * FROM {table_name} WHERE id={id}").fetchone()
     
     def retrieve_col_names(self, cursor, table_name):
-        # column method retrieval from stackoverflow on 07/02/2024: Author: 'do-me'
+        # column name method retrieval from stackoverflow on 07/02/2024: Author: 'do-me'
         # Available from: 
         """https://stackoverflow.com/questions/947215/how-to-get-a-list-of-column-names-on-sqlite3-database#:~:
         text=To%20find%20the%20column%20name,following%20code%20for%20the%20same.&text=This%20will%20print%20all
@@ -33,3 +39,30 @@ class DbReader():
     def retrieve_all_column_names(self, cursor, table_name):
         cursor.execute(F"SELECT * FROM {table_name}")
         return [name[0] for name in cursor.description][1:]
+    
+    # ----------------------------------------------------------------------------------
+    # Data Specific Functions
+
+    def retrieve_and_config_job_data(self, cursor, id):
+        
+        # retrieve all column names and raw job data incl. id's of linked tables
+        job_columns = self.retrieve_col_names(cursor, 'job_applications')
+        job_data = list(self.retrieve_single_row(cursor, id, 'job_applications'))
+
+        # correct data in job data from foreign key id to correct data name
+        job_data[7] = list(self.retrieve_single_col(cursor, id, 'type', 'employment_types'))[0]
+        job_data[8] = list(self.retrieve_single_col(cursor, id, 'duration', 'contract_period'))[0]
+        job_data[9] = list(self.retrieve_single_col(cursor, id, 'status', 'application_status'))[0]
+
+        # correct column names: id link to other table -> actual column name
+        job_columns[7] = "Employment Type"
+        job_columns[8] = "Employment Duration"
+        job_columns[9] = "Application Status"
+
+        # correct column name style to Title Format and remove underscores
+        for i in range(len(job_columns)):
+            job_columns[i] = job_columns[i].title().replace("_", " ")
+
+        # remove job id from data and zip column name and job data into single list
+        return list(zip(job_columns[1:], job_data[1:]))
+        
