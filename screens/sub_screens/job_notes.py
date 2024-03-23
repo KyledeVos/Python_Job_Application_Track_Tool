7f5b5adb36c49c5eb1a77b524b24bf5bc1949a1f
Kyle <kdevos827@gmail.com>
2024-03-23 17:01:12 +0200

    Added initializer and create_window_functions for window new window generation

--- screens/sub_screens/job_notes.py
+++ screens/sub_screens/job_notes.py
@@ -2,3 +2,20 @@ from ttkbootstrap import *
 from ttkbootstrap.scrolled import ScrolledText
 from ttkbootstrap.dialogs import Messagebox
 from datetime import date
+
+class NewJobNote():
+
+    def __init__(self, db_controller, job_note_attributes, job_id) -> None:
+
+        self.db_controller = db_controller
+        self.job_note_attributes = job_note_attributes
+        self.job_id = job_id
+
+    def create_note_window(self):
+
+        # Create New Window when adding job (to-do) note
+        self.note_window= Toplevel()
+        self.note_window.title("New Job Note")
+        self.note_window.geometry("500x600")
+        self.note_window.minsize(600, 550)
+        self.note_window.maxsize(700, 700)
