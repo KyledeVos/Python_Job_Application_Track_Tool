3b718c84b9f26dbe0f8c1ec0e30678881cccb471
Kyle <kdevos827@gmail.com>
2024-03-26 13:36:26 +0200

    Inital work on AllNotesView class with initializer retrieving job to_do notes summary info for display of notes summary data

--- screens/sub_screens/job_notes.py
+++ screens/sub_screens/job_notes.py
@@ -22,4 +22,28 @@ class NewJobNote(SubWindowBasic):
             super().__init__("Viewing Note", columns_categorized, all_columns, db_controller, 
                     single_data_list, boolean_data_list, large_box_data, fk_data,
                     btns_list, "Update Note", outer_window_reload_func,
-                    self.save_data, current_instance)
+                    self.save_note_data, current_instance)
+
+    def save_note_data():
+        print("save note data")
+
+# --------------------------------------------------------------------------------------
+class AllNotesView():
+
+    def __init__(self, outer_container, db_controller, job_id ) -> None:
+
+        self.outer_container = outer_container
+        self.db_controller = db_controller
+        self.job_id = job_id
+        
+        # retrieve all job to_do notes data for current job application
+        self.all_notes_summary_data = self.db_controller.retrieve_to_do_note_summary_data(job_id)
+        print(self.all_notes_summary_data)
+
+
+
+# --------------------------------------------------------------------------------------
+class NoteView():
+
+    def __init__(self) -> None:
+        pass
