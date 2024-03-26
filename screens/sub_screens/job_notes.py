ad620c3a4ec5a32c24390cf6a7803e920a4b6734
Kyle <kdevos827@gmail.com>
2024-03-26 23:41:27 +0200

    Initial functionality to display incomplete job-notes

--- screens/sub_screens/job_notes.py
+++ screens/sub_screens/job_notes.py
@@ -30,15 +30,80 @@ class NewJobNote(SubWindowBasic):
 # --------------------------------------------------------------------------------------
 class AllNotesView():
 
-    def __init__(self, outer_container, db_controller, job_id ) -> None:
+    def __init__(self, outer_container, db_controller, job_id, include_all = True ) -> None:
 
         self.outer_container = outer_container
         self.db_controller = db_controller
         self.job_id = job_id
         
         # retrieve all job to_do notes data for current job application
-        self.all_notes_summary_data = self.db_controller.retrieve_to_do_note_summary_data(job_id)
-        print(self.all_notes_summary_data)
+        # all_notes_summary form as {'columns' : [column names], 
+        #                           'values': [(values for note 1), (values for note 2)}
+
+        self.all_notes_summary_data = self.db_controller.retrieve_to_do_note_summary_data(job_id, include_all)
+
+
+    def load_all_to_do_notes(self):
+
+        # track placement of rows
+        notes_row_placement = 0
+        # track column placements for each row
+        column_placement = 0
+
+        # Check for present To-Do Notes data
+        if len(self.all_notes_summary_data['values']) == 0:
+
+            # NO DATA - PRINT MESSAGE
+            Label(self.outer_container, text="No To-Do Notes").grid(row=notes_row_placement, column = column_placement, padx=5, pady=5, sticky=W+E)
+        
+        else:
+          # DATA FOUND - DISPLAY DATA
+
+            # Add additional column for count of number of to_do notes
+            Label(self.outer_container, text="Number").grid(row=notes_row_placement, column = column_placement, padx=5, pady=5, sticky=W+E)
+            column_placement += 1
+
+            # load top row showing column names
+            for name in self.all_notes_summary_data['columns']:
+                title_label = Label(self.outer_container, text=name)
+                title_label.grid(row = notes_row_placement, column= column_placement, padx=5, pady=5, sticky=W+E)
+                column_placement += 1
+
+            # increment row placement for next row, reset column placements
+            notes_row_placement += 1
+            column_placement = 0
+
+            # place job notes data
+            for value_tup in self.all_notes_summary_data['values']:
+
+                # add Label for row count
+                count_label = Label(self.outer_container, text=notes_row_placement, width=15)
+                count_label.grid(row=notes_row_placement, column=column_placement, padx=5, pady=5, sticky=W+E)
+                column_placement += 1
+
+
+
+                for count, value in enumerate(value_tup):
+                    if count < len(value_tup) - 1:
+
+                        value_label = Label(self.outer_container, text=value, width=15)
+                        value_label.grid(row=notes_row_placement, column=column_placement, padx=5, pady=5, sticky=W+E)
+                        column_placement += 1
+
+                    else:
+                        # last value indicates state of complete boolean
+                        if value == 0:
+                            value_label = Label(self.outer_container, text="Incomplete")
+                            value_label.grid(row=notes_row_placement, column=column_placement, padx=5, pady=5, sticky=W+E)
+
+                        else:
+                            value_label = Label(self.outer_container, text="Complete")
+                            value_label.grid(row=notes_row_placement, column=column_placement, padx=5, pady=5,sticky=W+E)
+
+                # reset column placement for next row
+                column_placement = 0
+                
+                notes_row_placement += 1
 
 
 
